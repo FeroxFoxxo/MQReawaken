40313dc6b8306eb3bd9f8b686209e50200ae7066
feroxfoxxo <feroxfoxxo@gmail.com>
2022-11-30 15:45:19 +1300

    Added fallback in case of file explorer not opening.

--- Server.Reawakened/Launcher/Services/StartGame.cs
+++ Server.Reawakened/Launcher/Services/StartGame.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Server.Base.Core.Abstractions;
 using Server.Base.Core.Helpers;
 using Server.Reawakened.Launcher.Internal;
@@ -13,16 +14,20 @@ public class StartGame : IService
     private readonly LauncherConfig _lConfig;
     private readonly SettingsConfig _sConfig;
     private readonly IHostApplicationLifetime _appLifetime;
+    private readonly ILogger<StartGame> _logger;
     private readonly EventSink _sink;
     private Process _game;
 
+    public string CurrentVersion { get; private set; }
+
     public StartGame(EventSink sink, LauncherConfig lConfig, SettingsConfig sConfig,
-        IHostApplicationLifetime appLifetime)
+        IHostApplicationLifetime appLifetime, ILogger<StartGame> logger)
     {
         _sink = sink;
         _lConfig = lConfig;
         _sConfig = sConfig;
         _appLifetime = appLifetime;
+        _logger = logger;
     }
 
     public void Initialize()
@@ -36,9 +41,23 @@ public class StartGame : IService
 
     public void RunGame()
     {
-        if (!string.IsNullOrEmpty(_lConfig.GameSettingsFile))
-            _game = Process.Start(Path.Join(Path.GetDirectoryName(_lConfig.GameSettingsFile), "launcher",
-                "launcher.exe"));
+        if (string.IsNullOrEmpty(_lConfig.GameSettingsFile))
+        {
+            _logger.LogInformation("Please enter the absolute file path for your game's 'settings.txt' file.");
+            _lConfig.GameSettingsFile = Console.ReadLine();
+        }
+
+        var directory = Path.GetDirectoryName(_lConfig.GameSettingsFile);
+        _game = Process.Start(Path.Join(directory, "launcher", "launcher.exe"));
+        CurrentVersion = File.ReadAllText(Path.Join(directory, "current.txt"));
+
+        if (string.IsNullOrEmpty(_lConfig.CacheInfoFile))
+        {
+            _logger.LogInformation("Please enter the absolute file path for your cache's ROOT '__info' file.");
+            _lConfig.CacheInfoFile = Console.ReadLine();
+        }
+
+        _logger.LogInformation("Run game for process: {GamePath}", _game?.ProcessName);
     }
 
     private void GetGameInformation()
